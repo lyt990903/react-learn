a1f45eb51c27e93a57616abd9d49a8ae78382e10
lyt <lyt990903@outlook.com>
2023-08-13 20:15:53 +0800

    'finished'

---
+++ z_program/client/build/precache-manifest.d34ae41d66aff5ae6d4b941420f86a86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5439ccc24e6965bdef135fd1f3e61e49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "617578d39071e0d882b1",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "54b0e71ee3903699dd37",
+    "url": "/static/js/2.f5c23a7c.chunk.js"
+  },
+  {
+    "revision": "617578d39071e0d882b1",
+    "url": "/static/js/main.fb3904c3.chunk.js"
+  },
+  {
+    "revision": "a75b3db0e6676e2576ea",
+    "url": "/static/js/runtime-main.334ff1fe.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
